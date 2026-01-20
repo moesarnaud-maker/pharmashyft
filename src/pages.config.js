c588baced547008ef38a6113329c0add14493925
Arnaud Moës <moes.arnaud@gmail.com>
2026-01-20 14:35:00 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,15 +1,21 @@
 import EmployeeDashboard from './pages/EmployeeDashboard';
 import ManagerDashboard from './pages/ManagerDashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import Home from './pages/Home';
+import Kiosk from './pages/Kiosk';
+import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "EmployeeDashboard": EmployeeDashboard,
     "ManagerDashboard": ManagerDashboard,
     "AdminDashboard": AdminDashboard,
+    "Home": Home,
+    "Kiosk": Kiosk,
 }
 
 export const pagesConfig = {
     mainPage: "EmployeeDashboard",
     Pages: PAGES,
+    Layout: __Layout,
 };
